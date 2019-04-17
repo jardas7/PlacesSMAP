5eb19fefd137740d94745bd5a231e421bc93bd19
Jaromír Hnik <jardashnik@seznam.cz>
2019-04-17 17:25:50 +0200

    Initial Commit

---
+++ PlacesSMAP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlacesSMAP
+//
+//  Created by Jaromír Hnik on 17/04/2019.
+//  Copyright © 2019 Jaromír Hnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
